ef8f5aa245c7a00835bdecaa737ceb84532b6a59
reijjo <teemu.aitomeri@gmail.com>
2024-03-29 13:35:49 +0200

    toFixed2

--- client/components/homepage/Saldo.tsx
+++ client/components/homepage/Saldo.tsx
@@ -10,18 +10,18 @@ const Saldo = ({ ostot, myynnit, saldo }: SaldoProps) => {
       <h3 style={{ borderBottom: "1px solid black" }}>Kaikki</h3>
       <div className="ostot">
         <h4>Ostot</h4>
-        <p>{ostot} &euro;</p>
+        <p>{ostot.toFixed(2)} &euro;</p>
       </div>
       <div className="myynnit">
         <h4>Myynnit</h4>
-        <p>{myynnit} &euro;</p>
+        <p>{myynnit.toFixed(2)} &euro;</p>
       </div>
       <div className="total">
         <h3>Saldo</h3>
         <p>
           <strong className={saldo > 0 ? `plussateksti` : `miinusteksti`}>
             {saldo > 0 && "+"}
-            {saldo} &euro;
+            {saldo.toFixed(2)} &euro;
           </strong>
         </p>
       </div>
